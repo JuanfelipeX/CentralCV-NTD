a2eaf6eabdbd0e0d47c1e4be7539a5e9f788670b
cristin corso <cristianc427@gmail.com>
2022-04-19 15:48:42 -0500

    encriptar

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,15 @@
-const express =require("express");
-const animalSchema = require("../models/animal");
-
-const router = express.Router();
-
-router.post("/user",(req,res)=>{
-    const user =userSchema(req.body);
-    user
-        .save()
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error}));
+const express = require("express");
+const router = express.Router(); //manejador de rutas de express
+const userSchema = require("../models/user");
+router.post('/signup', async (req, res) => {
+    const { usuario, correo, clave } = req.body;
+    const user = new userSchema({
+        usuario: usuario,
+        correo: correo,
+        clave: clave
     });
+    user.clave = await user.encryptClave(user.clave);
+    await user.save(); //save es un método de mongoose para guardar datos en MongoDB 
+    res.json(user);
+});
+module.exports = router;
