2e68f654e6d6b7a5063e3de5df5b5e406c4310a7
JuanDiaz <juane.diazt@konradlorenz.edu.co>
2022-04-20 22:12:39 -0500

    Agregados archivos endpoints
    Se agregan todos los archivos endpoint, para poder trabajarlos

---
+++ src/models/cuenta.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose"); // importando el componente mongoose
+const bcrypt = require("bcrypt"); // importando el componente bcrypt
+const studiesSchema = mongoose.Schema({
+    nivelEstudios: {
+        type: String,
+        required: true,
+    },
+    titulos: {
+        type: Array,
+        required: true,
+    },
+    descripcion: {
+        type: String,
+        required: true,
+    },
+    centrosEducativos: {
+        type: Array,
+        required: true,
+    },
+    duracion: {
+        type: String,
+        required: true,
+    },
+    lugar: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model("studies", studiesSchema);
