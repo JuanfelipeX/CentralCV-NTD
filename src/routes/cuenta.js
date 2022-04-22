129b294f76c7572ae54d40b201e310c1d0d77a0b
JuanDiaz <juane.diazt@konradlorenz.edu.co>
2022-04-22 18:58:05 -0500

    Primera parte del CRUD de cuenta
    Se hizo la primera parte del CRUD de cuenta.

--- src/routes/cuenta.js
+++ src/routes/cuenta.js
@@ -4,26 +4,54 @@ const bcrypt = require("bcrypt");
 const router = express.Router(); //manejador de rutas de express
 const cuentaSchema = require("../models/cuenta");
 
+//cuenta post
+router.post("/cuenta", (req, res) => {
+    const cuenta = cuentaSchema(req.body);
 
-/*
+    cuenta
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
 
-poner todos los metodos http de authentication de studies en este archivo /routes/studies, 
-pero no deja ya que toca arreglar las routes y app.use del index.js pero a la hora de modificarlo y arreglarlo no deja 
-y aparce un error, lo he intentado pero aparece el error app crashed y sale error de throw(new ...) revisarlo y preguntar como arreglarlo
 
-*/
+//studies get
+router.get("/cuenta", (req, res) => {
+    cuentaSchema
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
 
-router.post("/cuenta", async(req, res) => {
-    const { nombre, apellidos, email, puestoDeTrabajo, departamento} =
+
+//studies get por id
+router.get("/cuenta/:id", (req, res) => {
+    const { id } = req.params;
+    cuentaSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//studies actualizar datos por id
+router.put("/estudio/:id", (req, res) => {
+    const { id } = req.params;
+    const { nivelEstudios, titulos, descripcion, centrosEducativos, duracion, lugar } =
     req.body;
+    estudioSchema
+        .updateOne({ _id: id }, {
+            $set: { nivelEstudios, titulos, descripcion, centrosEducativos, duracion, lugar }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
 
-    const studies = new cuentaSchema({
-        nombre: nombre,
-        apellidos: apellidos,
-        email: email,
-        puestoDeTrabajo: puestoDeTrabajo,
-        departamento: departamento
-    });
 
-    await cuenta.save();
+//studies eliminar por id
+router.delete("/estudio/:id", (req, res) => {
+    const { id } = req.params;
+    estudioSchema
+        .remove({ _id: id })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
 });
