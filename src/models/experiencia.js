ad02f270be71456ecbc6f395f5d97576e20c5be1
JuanDiaz <juane.diazt@konradlorenz.edu.co>
2022-04-21 20:51:19 -0500

    Se cambia y corrigen errores
    Se corrigieron varios errores

--- src/models/experiencia.js
+++ src/models/experiencia.js
@@ -1,30 +1,18 @@
 const mongoose = require("mongoose"); // importando el componente mongoose
 const bcrypt = require("bcrypt"); // importando el componente bcrypt
-const studiesSchema = mongoose.Schema({
-    nivelEstudios: {
+const experienciaSchema = mongoose.Schema({
+    cargo: {
         type: String,
         required: true,
     },
-    titulos: {
-        type: Array,
-        required: true,
-    },
-    descripcion: {
+    nombreEmpresa: {
         type: String,
-        required: true,
-    },
-    centrosEducativos: {
-        type: Array,
         required: true,
     },
     duracion: {
         type: String,
         required: true,
     },
-    lugar: {
-        type: String,
-        required: true,
-    },
 });
 
-module.exports = mongoose.model("studies", studiesSchema);
+module.exports = mongoose.model("experiencia", experienciaSchema);
