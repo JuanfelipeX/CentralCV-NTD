5c039db47006283dcb19347abd93f5ff61162e7d
JuanDiaz <juane.diazt@konradlorenz.edu.co>
2022-04-22 21:01:05 -0500

    Se hizo el CRUD de experiencia

--- src/routes/experiencia.js
+++ src/routes/experiencia.js
@@ -1,27 +1,57 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const router = express.Router(); //manejador de rutas de express
+const router2 = express.Router(); //manejador de rutas de express
 const experienciaSchema = require("../models/experiencia");
 
+//experiencia post
+router2.post("/experiencia", (req, res) => {
+    const experiencia = experienciaSchema(req.body);
 
-/*
+    experiencia
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
 
-poner todos los metodos http de authentication de studies en este archivo /routes/studies, 
-pero no deja ya que toca arreglar las routes y app.use del index.js pero a la hora de modificarlo y arreglarlo no deja 
-y aparce un error, lo he intentado pero aparece el error app crashed y sale error de throw(new ...) revisarlo y preguntar como arreglarlo
 
-*/
+//experiencia get
+router2.get("/experiencia", (req, res) => {
+    experienciaSchema
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
 
-router.post("/experiencia", async(req, res) => {
+
+//experiencia get por id
+router2.get("/experiencia/:id", (req, res) => {
+    const { id } = req.params;
+    experienciaSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//experiencia actualizar datos por id
+router2.put("/experiencia/:id", (req, res) => {
+    const { id } = req.params;
     const { cargo, nombreEmpresa, duracion } =
     req.body;
+    experienciaSchema
+        .updateOne({ _id: id }, {
+            $set: {  cargo, nombreEmpresa, duracion }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
 
-    const studies = new experienciaSchema({
-        cargo: cargo,
-        nombreEmpresa: nombreEmpresa,
-        duracion: duracion
-    });
 
-    await experiencia.save();
+//experiencia eliminar por id
+router2.delete("/experiencia/:id", (req, res) => {
+    const { id } = req.params;
+    experienciaSchema
+        .remove({ _id: id })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
 });
