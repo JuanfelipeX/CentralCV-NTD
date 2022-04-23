b6d6fa53b70d5e47d0709e2c6d99dc9f81a0bc15
JuanDiaz <juane.diazt@konradlorenz.edu.co>
2022-04-22 20:46:19 -0500

    Se hizo el CRUD de datosPersonales
    Se ha hecho el CRUD de datosPersonales.

--- src/routes/datosPersonales.js
+++ src/routes/datosPersonales.js
@@ -5,28 +5,54 @@ const router = express.Router(); //manejador de rutas de express
 const datosPersonalesSchema = require("../models/datosPersonales");
 
 
-/*
+//datosPersonales post
+router.post("/datosPersonales", (req, res) => {
+    const datosPersonales = datosPersonalesSchema(req.body);
 
-poner todos los metodos http de authentication de studies en este archivo /routes/studies, 
-pero no deja ya que toca arreglar las routes y app.use del index.js pero a la hora de modificarlo y arreglarlo no deja 
-y aparce un error, lo he intentado pero aparece el error app crashed y sale error de throw(new ...) revisarlo y preguntar como arreglarlo
+    datosPersonales
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
 
-*/
 
-router.post("/datosPersonales", async(req, res) => {
-    const { fechaNacimiento, identificacion, telefono, pais, departamento, ciudad, genero, perfilProfesional} =
+//datosPersonales get
+router.get("/datosPersonales", (req, res) => {
+    datosPersonalesSchema
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+
+//datosPersonales get por id
+router.get("/datosPersonales/:id", (req, res) => {
+    const { id } = req.params;
+    datosPersonalesSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//datosPersonales actualizar datos por id
+router.put("/datosPersonales/:id", (req, res) => {
+    const { id } = req.params;
+    const { fechaNacimiento, identificacion, telefono, pais, departamento, ciudad, genero, perfilProfesional } =
     req.body;
+    datosPersonalesSchema
+        .updateOne({ _id: id }, {
+            $set: { fechaNacimiento, identificacion, telefono, pais, departamento, ciudad, genero, perfilProfesional }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
 
-    const studies = new datosPersonalesSchema({
-        fechaNacimiento: fechaNacimiento,
-        identificacion: identificacion,
-        telefono: telefono,
-        pais: pais,
-        departamento: departamento,
-        ciudad: ciudad,
-        genero: genero,
-        perfilProfesional: perfilProfesional
-    });
 
-    await datosPersonales.save();
+//datosPersonales eliminar por id
+router.delete("/datosPersonales/:id", (req, res) => {
+    const { id } = req.params;
+    datosPersonalesSchema
+        .remove({ _id: id })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
 });
