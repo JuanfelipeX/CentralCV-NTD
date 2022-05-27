939ed83d29849cd7bb5d71b8e33933d1923ee72d
JuanDiaz <juane.diazt@konradlorenz.edu.co>
2022-05-27 12:08:24 -0500

    Added private URL

--- cv/src/app/services/cv.service.ts
+++ cv/src/app/services/cv.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CvService {
-
-  constructor() { }
+  private URL = 'http://localhost:3000/api';
+  
+  constructor(private http: HttpClient) { }
 }
